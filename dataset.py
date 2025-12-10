7921dd40081505c0d43a85a392641077e699b0b9
Ayesha Qureshi <a.qureshi0327@gmail.com>
2025-12-10 17:15:17 -0500

    baselineCNN created and executed

---
+++ dataset.py
@@ -0,0 +1,44 @@
+"""
+Custom dataset class that tells PyTorch:
+  - how to find images
+  - how to read them
+  - how to apply transforms
+  - how to return (image, label)
+"""
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+
+class PlantVillageDataset(Dataset):
+  def __init__(self, root_dir, classes, transform=None):
+    self.root_dir = root_dir
+    self.transform = transform
+    self.classes = classes
+
+    self.image_paths = []
+    self.labels = []
+
+    for idx, cls in enumerate(classes):
+      class_path = os.path.join(root_dir, cls)
+      img_files = os.listdir(class_path)
+
+      for img_name in img_files:
+        self.image_paths.append(os.path.join(class_path, img_name))
+        self.labels.append(idx)
+
+  def __len__(self):
+    return len(self.image_paths)
+  
+  def __getitem__(self,index):
+
+    #Step 1 -> load image
+    img_path = self.image_paths[index]
+    img = Image.open(img_path).convert("RGB")
+
+    #Step 2 -> apply transform
+    if self.transform:
+      img = self.transform(img)
+    
+    #Step 3 -> return (img, label)
+    label = self.labels[index]
+    return (img, label)
